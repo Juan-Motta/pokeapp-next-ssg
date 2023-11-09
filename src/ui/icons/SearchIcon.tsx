3bda6a36341a17169a4534b414d742de025cedad
Juan Motta <juanandres.lopez@crehana.com>
2023-11-09 17:25:36 -0500

    feat: added main layout component

---
+++ src/ui/icons/SearchIcon.tsx
@@ -0,0 +1,10 @@
+export default function SearchIcon() {
+    return (
+        <svg viewBox="0 0 24 24">
+            <path
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                className="stroke-[2px] stroke-gray-400"
+            />
+        </svg>
+    );
+}
---
+++ src/ui/layouts/MainLayout.tsx
@@ -0,0 +1,40 @@
+import Head from 'next/head';
+
+import Navbar from '../components/Navbar';
+
+interface Props {
+    children: React.ReactNode;
+    title?: string;
+    description?: string;
+    keywords?: string;
+    image?: string;
+}
+
+export default function MainLayout({
+    children,
+    title,
+    description,
+    keywords,
+    image,
+}: Props): React.ReactNode {
+    return (
+        <>
+            <Head>
+                <title>{title || 'PokemonApp'}</title>
+                <meta name="author" content="Juan Motta" />
+                <meta
+                    name="description"
+                    content={`Information about pokemon ${title}`}
+                />
+                <meta
+                    name="keywords"
+                    content={`${title}, pokemon, pokedex, pokeapp, pokeapi`}
+                />
+            </Head>
+            <div className="container mx-auto overflow-hidden">
+                <Navbar />
+                <main>{children}</main>
+            </div>
+        </>
+    );
+}
