9420a4371f75c663540ffbee01f9c4184d73238e
Juan Motta <juanandres.lopez@crehana.com>
2023-11-12 17:22:14 -0500

    chore: added config for next images

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ const nextTranslate = require('next-translate-plugin');
 module.exports = nextTranslate({
     reactStrictMode: true,
     output: 'standalone',
+    images: {
+        domains: ['assets.pokemon.com'],
+        formats: ['image/avif', 'image/webp'],
+    },
 });
